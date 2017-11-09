e4b2996a5bf4ef1a1ef8afccfddc74ed514c9d91
maartendegen <maartendegen@gmail.com>
2017-11-09 13:15:40 +0100

    script.py

--- script.py
+++ script.py
@@ -1,1 +1,6 @@
+import test
 print("This is a test print")
+
+print("This is the circle circumference for a cirle of rad 5:", test.circum(5))
+
+print("This is the area of a circle of rad 5:",test.area(5))
