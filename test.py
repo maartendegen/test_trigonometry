88ca074643f0a614b718cf43ef3a7f89bc306f27
maartendegen <maartendegen@gmail.com>
2017-11-09 13:00:40 +0100

    Added function to calc circle area

--- test.py
+++ test.py
@@ -9,3 +9,8 @@ input_rad = float(input("Please give me the number of your circle:"))
 
 print("The circumference of your circle is: ", circum(input_rad))
 
+
+def area(radius):
+	return np.pi*radius**2
+
+print("The area of your circle is: ", area(input_rad))
