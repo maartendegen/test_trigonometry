31278cb31237d786c1367e2100ff28126c1ae465
maartendegen <maartendegen@gmail.com>
2017-11-09 12:56:54 +0100

    Function to calc circle circumference added

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
+import numpy as np
+
 print('Hello world')
+
+def circum(radius):
+	return 2*np.pi*radius
+
+input_rad = float(input("Please give me the number of your circle:"))
+
+print("The circumference of your circle is: ", circum(input_rad))
+
